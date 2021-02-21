704f5928d72d333b9163436ef9263c0afcf1a72f
unknown <phera5432@naver.com>
2021-02-21 17:27:17 +0900

    Initialize Project

---
+++ indie/src/test/java/com/mindoo/indie/IndieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindoo.indie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
